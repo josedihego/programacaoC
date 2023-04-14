6453c3cfb45468dd22987ce222a191779626a099
josedihego <josedihego@gmail.com>
2023-04-14 12:20:09 -0300

    go

--- prog_02_B.c
+++ prog_02_B.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+// pow(x,y)
 
 int somar(int v1, int v2){
     int resultado = v1 + v2;
+    return resultado;
+}
+int multiplicar(int v1, int v2){
+    int resultado =  v1 * v2;
+    return resultado;
+}
+int potenciar(int v1, int v2){
+    int resultado = pow(v1,v2);
     return resultado;
 }
 
@@ -14,8 +24,11 @@ int main(){
     printf("%s", "Informe o valor de y: ");
     scanf("%d", &y);
     int res_soma = somar(x,y);
-    printf("x= %d, y = %d ", x,y);
     printf("Resultado da soma : %d\n", res_soma);
-
-
+    int res_mult = multiplicar(x,y);
+    printf("Resultado multiplicação: %d\n", res_mult);
+    int res_pot = potenciar(x,y);
+    printf("Resultado potenciar: %d\n", res_pot);
+    
+    return EXIT_SUCCESS;
 }
