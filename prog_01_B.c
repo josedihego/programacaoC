71379586811e91d1d39da67f4b5607b16c1dc18e
josedihego <josedihego@gmail.com>
2023-04-14 11:47:07 -0300

    11:46 14/04

--- prog_01_B.c
+++ prog_01_B.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+// exercício fazer a subtração  65 - 34
+// divisão  65/34
+// potenciação 65 elevado a 34
 int soma(int v1, int v2){
     int resultado = v1+v2;
+    return resultado;
+}
+int potenciacao(int v1, int v2){
+    int resultado = pow(v1,v2);
     return resultado;
 }
 
@@ -12,8 +20,10 @@ int multiplicacao(int v1, int v2){
 }
 
 int main(){
-    int x = 15;
-    int y = 20;
+    int x;
+    int y;
+    scanf("%d",&x);
+    scanf("%d", &y);
     int res_soma = soma(x,y);
     printf("Resultado soma: %d\n",res_soma);
     int res_mult = multiplicacao(x,y);
