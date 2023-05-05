faf6c3b44804a8cb580daa4ceb0fd7250499462e
josedihego <josedihego@gmail.com>
2023-05-05 09:20:52 -0300

    quimica

---
+++ automacao/aut_B.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    float valor = 0;
+    float media;
+    float somatorio = 0;
+    int qnt = 0;
+    while (somatorio < 15000)
+    {
+        printf("%s", "Informe o valor da venda:");
+        scanf("%f", &valor);
+    }
+    
+}
