f479259af269771d7b96b2421a093ba11885d3f6
josedihego <josedihego@gmail.com>
2023-05-05 09:20:42 -0300

    quimica

---
+++ quimica/prog_A.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+
+// vf =  vi + a * t
+// delta = b² - 4 * a * c
+int main(){
+    float vf;
+    float vi;
+    float a;
+    float t;
+    printf("%s", "Informe a velocidade vi:");
+    scanf("%f", &vi);
+    printf("%s", "Informe a aceleração:");
+    scanf("%f", &a);
+    printf("%s", "Informe o valor do tempo:");
+    scanf("%f", &t);
+    vf = vi + (a * t);
+    printf("Velocidade final %f m/s\n", vf);
+
+   float a, b, c;
+   float delta;
+   delta = pow(b,c) - (4 * a * c);
+    
+}
