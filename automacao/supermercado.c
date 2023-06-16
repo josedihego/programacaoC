a3fa35d22ecaf6cfc8fa7ddfe441b99596814a67
josedihego <josedihego@gmail.com>
2023-06-16 08:27:39 -0300

    com matrizes

---
+++ automacao/supermercado.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+// 0. farinha
+// 1. 
+int main(){
+    int * quantidades = malloc(500* sizeof(int));
+    float * precos  = malloc(500 * sizeof(float));
+
+}
