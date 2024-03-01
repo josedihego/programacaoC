872d5cae88585d6375e3cf1ff5e08d2514a43ee4
Jose Dihego <josedihego@gmail.com>
2024-03-01 10:15:32 -0300

    bhaskara

--- 2024_quimica/bhaskara.c
+++ 2024_quimica/bhaskara.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
+#include<math.h>
 
-float a = 3.2;
-float b = 4.5;
-float c = 5.6;
+int main(){
+    float a, b, c;
+    printf("Informe o valor de a:");
+    scanf("%f",&a);
+    printf("Informe o valor de b:");
+    scanf("%f",&b);
+    printf("Informe o valor de c:");
+    scanf("%f",&c);
+
+    float delta = (b * b) - (4 * a * c);
+    if(delta > 0){
+        float x1 = (-1 * b + sqrt(delta))/(2 * a);
+        float x2 = (-1 * b - sqrt(delta))/(2 * a);
+    }
+    else{
+
+    }
+}
