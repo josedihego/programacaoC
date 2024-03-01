8116fec4131235eb1742e90b7d3ba541f1c00561
Jose Dihego <josedihego@gmail.com>
2024-03-01 07:33:49 -0300

    ok

--- 2024_automacao/imc.c
+++ 2024_automacao/imc.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
 float altura = 1.60;
-float massa = 44.44;
+float massa = 60.3;
 float gravidade = 9.81;
 
 int main() {
   float IMC = massa / (altura * altura);
-  printf("Seu IMC: %.2f", IMC);
+  printf("Seu IMC: %.2f\n", IMC);
   float peso = massa * gravidade;
-  printf("Seu peso: %.2f", peso);
+  printf("Seu peso: %.2f\n", peso);
+  if(IMC < 18.5){
+    printf("Abaixo do peso.");
+  }
+  else{
+    if(IMC >= 18.5 && IMC < 25){
+        printf("Peso normal.");
+    }
+    else{
+
+    }
+  }
 }
