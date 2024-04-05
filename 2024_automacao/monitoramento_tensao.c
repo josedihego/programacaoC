ac5ec4aadc11d4ce707e875f833e82b03e5fe30a
Jose Dihego <josedihego@gmail.com>
2024-04-05 07:57:20 -0300

    git

---
+++ 2024_automacao/monitoramento_tensao.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define NUM_VER_DIA 24*60*6 // OPÇÃO 1
+
+int main(){
+    for(int i =0; i < NUM_VER_DIA;  i = i +1){
+
+    }
+}
---
+++ 2024_automacao/relembrando_for.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    for(int i = 0 ; i < 20; i = i +1){
+        printf("%d\n",i);
+    }
+}
