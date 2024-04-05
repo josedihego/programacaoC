36626440308b4846e1ebf0ea6b3d6f980a647204
Jose Dihego <josedihego@gmail.com>
2024-04-05 09:34:18 -0300

    vai

---
+++ 2024_quimica/monitoramento_maquina.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define NUM_LEITURAS 24*60*6
+
+int main(){
+    for(int i =0 ; i < NUM_LEITURAS ; i = i +1){
+        //ler a voltagem
+        //testar se esta entre 100V e 250V com um if
+        // se estiver incrementar a quantidade
+    }
+    // depois do for
+    // calcular a percentagem
+    // imprimir essa percentagem
+}
