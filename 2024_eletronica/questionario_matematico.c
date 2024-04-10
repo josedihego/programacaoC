627da875a04bab14edd4c00102b88929fb6c6f46
josedihego@gmail.com <josedihego@gmail.com>
2024-04-10 08:17:59 -0300

    vai

---
+++ 2024_eletronica/questionario_matematico.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+
+
+int main(){
+    srand(time(0));
+    int x = (rand() % 100) + 1;
+    int y = (rand() % 100) + 1;
+    int n_erros = 0;
+    int n_tentativas = 0;
+    int n_vidas = 2;
+    while(n_vidas > 0){
+        int r1, r2;
+        int g1, g2;
+        printf("Qual o valor de %d + %d?",x,y);
+        scanf("%d",&r1);
+        g1 = x+y;
+        if(x>y){
+            printf("Qual o valor de %d / %d?", x,y);
+            g2 = x/y;
+        }
+        else{
+            printf("Qual o valor de %d / %d?", y,x);
+            g2 =  y/x;
+
+        }
+        scanf("%d",&r2);
+    }
+}
