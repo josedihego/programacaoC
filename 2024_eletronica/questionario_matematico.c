0d69eed20237f06f62e8869fe404393ee9acceab
josedihego@gmail.com <josedihego@gmail.com>
2024-04-10 08:31:47 -0300

    vai

--- 2024_eletronica/questionario_matematico.c
+++ 2024_eletronica/questionario_matematico.c
@@ -27,5 +27,17 @@ int main(){
 
         }
         scanf("%d",&r2);
+        if(r1!=g1 || r2!=g2){
+            n_erros = n_erros +1;
+            n_vidas = n_vidas - 1;
+        }
+        else{
+            n_vidas = 2;
+        }
+        n_tentativas = n_tentativas + 1; 
+        x = (rand() % 100) + 1;
+        y = (rand() % 100) + 1;
     }
+    printf("Você errou duas seguidas :(\n");
+    printf("Você respondeu %d perguntas e errou %d", n_tentativas, n_erros);
 }
