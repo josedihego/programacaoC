597bf7608d81653184bc246d731b634e23c03ce5
josedihego@gmail.com <josedihego@gmail.com>
2024-04-12 09:57:28 -0300

    .

---
+++ 2024_quimica/aleatorio.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+
+int main(){
+    srand(time(0));
+    for(int i = 0; i < 10; i = i+1){
+        int aleatorio = rand() % 101;
+        printf("%d\n", aleatorio);
+        sleep(5);
+    }
+}
