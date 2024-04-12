d0c5693dad2863d393a51c68ce0188a7a1c412bc
josedihego@gmail.com <josedihego@gmail.com>
2024-04-12 08:31:47 -0300

    complete

--- 2024_automacao/jogo_matematico_while.c
+++ 2024_automacao/jogo_matematico_while.c
@@ -6,8 +6,14 @@
 
 
 int main(){
+    srand(time(0));
     bool acertou = true;
     while(acertou){
-
+        int x = rand() % 201;
+        int y = rand() % 201;
+        int resposta;
+        printf("Qual o valor de %d+%d?\n",x,y);
+        scanf("%d",&resposta);
+        if(x+y != resposta) acertou = false;
     }
 }
