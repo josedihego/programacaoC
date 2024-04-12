86a6f714412029d791800c8f7ea4499d367dcc43
josedihego@gmail.com <josedihego@gmail.com>
2024-04-12 11:35:29 -0300

    .

---
+++ 2024_mecanica/soma_aleatorio.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    srand(time(0));
+    int n1, n2, resposta;
+    int n_acertos=0, n_erros=0;
+    for(int i=0; i < 10; i = i +1){
+        //gerar dois valores aleatórios n1 e n2
+        // perguntar ao usuário quanto é n1+n2?
+        // ler a resposta com scanf 
+        // comparar a resposat com o valor correto
+        // usar um if para comparação
+        // se esta correto você conta um acerto
+        // de esta errado você conta um erro        
+    }
+    // depois do for, no final imprime a percentagem de acertos
+}
