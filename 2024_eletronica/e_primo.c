7665f84ba75e0c2485f29e42f81468f8d3ca9ab5
josedihego@gmail.com <josedihego@gmail.com>
2024-04-17 08:11:57 -0300

    go

---
+++ 2024_eletronica/e_primo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int numero;
+    printf("Informe um número para testar se é primo:");
+    scanf("%d",&numero);
+    // usar um while para testar todos os numeros entre 2 e numero-1
+    //até encontrar um que divida o numero testado OU que você
+    // não encontre esse número
+}
