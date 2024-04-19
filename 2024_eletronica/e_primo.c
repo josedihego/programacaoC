f09d02aa949bba6aa0dfd2d282375e5f16140256
josedihego@gmail.com <josedihego@gmail.com>
2024-04-19 08:04:31 -0300

    sim

--- 2024_eletronica/e_primo.c
+++ 2024_eletronica/e_primo.c
@@ -5,7 +5,16 @@ int main(){
     int numero;
     printf("Informe um número para testar se é primo:");
     scanf("%d",&numero);
-    // usar um while para testar todos os numeros entre 2 e numero-1
-    //até encontrar um que divida o numero testado OU que você
-    // não encontre esse número
+    int n_divisores = 2;
+    for(int i = 2; i < numero; i = i +1){
+        if(numero%i==0){
+            n_divisores = n_divisores +1;
+        }
+    }
+    if(n_divisores==2){
+        printf("O número %d é primo\n",numero);
+    }
+    else{
+        printf("O número %d NÃO é primo\n", numero);
+    }
 }
