5cd85224beb505521e01a223b212c99b56b8edad
josedihego@gmail.com <josedihego@gmail.com>
2024-04-19 10:09:55 -0300

    go

--- 2024_quimica/jogo_matematico.c
+++ 2024_quimica/jogo_matematico.c
@@ -1,17 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
-int main(){
+int main()
+{
     srand(time(0));
     bool acertou = true;
-    while(acertou){
+    int qnt = 0;
+    while (acertou)
+    {
+        qnt = qnt + 1;
         int n1 = rand() % 101;
         int n2 = rand() % 101;
         printf("Qual o valor de %d + %d ?", n1, n2);
         int resposta;
-        scanf("%d",&resposta);
-        if(resposta != n1+n2) acertou = false;
+        scanf("%d", &resposta);
+        if (resposta != n1 + n2)
+        {
+            acertou = false;
+            printf("A resposta certa é: %d \n", n1 + n2);
+        }
     }
+    printf("Você tentou %d vezes \n",qnt);
 }
