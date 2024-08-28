f22b5f4409e1a3724ea345fa5ff9e9087d91f8f1
josedihego@gmail.com <josedihego@gmail.com>
2024-08-28 08:47:27 -0300

    com arrays

--- 2024_eletronica/arrays_basico2.c
+++ 2024_eletronica/arrays_basico2.c
@@ -17,4 +17,17 @@ int main(){
     }
     printf("Maiores de idade: %d \n", qnt_maiores);
     printf("Menores de idade: %d \n",13-qnt_maiores);
+    
+    int mais_velho = idades[0];
+    int mais_nova = idades[0];
+    for(int i =1; i < 13; i = i +1){
+        if(idades[i] > mais_velho){
+            mais_velho = idades[i];
+        }
+        if(idades[i] < mais_nova){
+            mais_nova  = idades[i];
+        }
+    }
+    printf("Idade da pessoa mais velha: %d\n",mais_velho);
+    printf("Idade da pessoa mais jovem é: %d \n", mais_nova);
 }
