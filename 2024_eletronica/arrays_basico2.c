f196eba9d339a4e7208614d4c6a28b2ef97b2bee
josedihego@gmail.com <josedihego@gmail.com>
2024-09-04 07:33:33 -0300

    com QNT

--- 2024_eletronica/arrays_basico2.c
+++ 2024_eletronica/arrays_basico2.c
@@ -3,26 +3,28 @@
 #include <math.h>
 #include <stdbool.h>
 
+#define QNT 100
+
 int main(){
-    int idades [13];
-    for(int i =0; i < 13; i = i +1){
+    int idades [QNT];
+    for(int i =0; i < QNT; i = i +1){
         printf("Informe a idade da pessoa n° %d: ",i+1);
         scanf("%d", &idades[i]);
     }
     int qnt_maiores = 0;
-    for(int i =0; i < 13; i = i +1){
+    for(int i =0; i < QNT; i = i +1){
         if(idades[i] >= 18){
             qnt_maiores = qnt_maiores +1;
         }
     }
     printf("Maiores de idade: %d \n", qnt_maiores);
-    printf("Menores de idade: %d \n",13-qnt_maiores);
+    printf("Menores de idade: %d \n",QNT-qnt_maiores);
     
     int mais_velho = idades[0];
     int mais_nova = idades[0];
     int p_mv = 0;
     int p_mn = 0;
-    for(int i =1; i < 13; i = i +1){
+    for(int i =1; i < QNT; i = i +1){
         if(idades[i] > mais_velho){
             mais_velho = idades[i];
             p_mv = i;
