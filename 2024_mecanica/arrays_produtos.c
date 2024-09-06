2d9a818672cb4bafd89690f4291df161107de19a
josedihego@gmail.com <josedihego@gmail.com>
2024-09-06 11:07:49 -0300

    go

--- 2024_mecanica/arrays_produtos.c
+++ 2024_mecanica/arrays_produtos.c
@@ -5,5 +5,13 @@
 
 
 int main(){
-    
+    char nomes_produtos [20][70];
+    int qnts_produtos [20];
+    printf("Informe uma das opções:");
+    printf("\n\t 1.Cadastrar produto");
+    printf("\n\t 2.Buscar produto");
+    printf("\n\t 3.Listar produtos");
+    printf("\n\t 4.Remover produtos");
+    printf("\n\t 5.Sair\n");
+
 }
