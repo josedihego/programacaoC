4146df395f4016c5b04e656e293ae7d8affa4f81
josedihego@gmail.com <josedihego@gmail.com>
2024-09-06 11:30:28 -0300

    go

--- 2024_mecanica/arrays_produtos.c
+++ 2024_mecanica/arrays_produtos.c
@@ -13,5 +13,28 @@ int main(){
     printf("\n\t 3.Listar produtos");
     printf("\n\t 4.Remover produtos");
     printf("\n\t 5.Sair\n");
+    int opcao;
+    scanf("%d", &opcao);
+    switch(opcao){
+        case 1:
+            printf("Cadastrar selecionando\n");
+            break;
+        case 2:
+            printf("Buscar selecionando\n");
+            break;
+        case 3:
+            printf("Listar selecionando\n");
+            break;
+        case 4:
+            printf("Remover selecionando");
+            break;
+        case 5:
+            printf("Sair selecionando\n");
+            break;
+        default:
+            printf("Opção inválida. Tente novamente.");
+            break;    
+
+    }
 
 }
