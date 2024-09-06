c9c22260df7712d44afee429b4a0d8e3e4a5a49d
josedihego@gmail.com <josedihego@gmail.com>
2024-09-06 07:58:48 -0300

    final arrays basico 2

--- 2024_automacao/arrays_basico2.c
+++ 2024_automacao/arrays_basico2.c
@@ -3,7 +3,7 @@
 #include<math.h>
 #include<stdbool.h>
 
-#define NA 8
+#define NA 5
 int main(){
     int idades[NA];
     for(int i =0; i < NA; i = i +1){
@@ -14,8 +14,23 @@ int main(){
     for(int i=0; i < NA; i = i +1){
         soma =  soma + idades[i];
     }
-    printf("A soma das idades é: %d", soma);
+    printf("A soma das idades é: %d\n", soma);
+    printf("A média de idades é: %d\n", soma/NA);
 
+    int mais_experiente = idades[0];
+    for(int i=1; i < NA; i = i +1){
+        if(mais_experiente < idades[i]){
+            mais_experiente =  idades[i];
+        }
+    }
+    int mais_jovem = idades[0];
+    for(int i=1; i < NA; i = i +1){
+        if(mais_jovem > idades[i]){
+            mais_jovem = idades[i];
+        }
+    }
+    printf("Idade do aluna(o) mais experinte é: %d\n", mais_experiente);
+    printf("Idade do aluna(o) mais jovem é: %d \n", mais_jovem);
 }
 
 
