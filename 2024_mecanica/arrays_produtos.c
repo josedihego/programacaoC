c1dfa904646f6ee9265647c91f5489365fc9fedd
josedihego@gmail.com <josedihego@gmail.com>
2024-09-06 11:57:53 -0300

    go

--- 2024_mecanica/arrays_produtos.c
+++ 2024_mecanica/arrays_produtos.c
@@ -7,16 +7,16 @@ int main()
 {
     char nomes_produtos[20][70];
     int qnts_produtos[20];
-    printf("Informe uma das opções:");
-    printf("\n\t 1.Cadastrar produto");
-    printf("\n\t 2.Buscar produto");
-    printf("\n\t 3.Listar produtos");
-    printf("\n\t 4.Remover produtos");
-    printf("\n\t 5.Sair\n");
     int proxPL = 0;
     int opcao;
     while (opcao != 5)
     {
+        printf("Informe uma das opções:");
+        printf("\n\t 1.Cadastrar produto");
+        printf("\n\t 2.Buscar produto");
+        printf("\n\t 3.Listar produtos");
+        printf("\n\t 4.Remover produtos");
+        printf("\n\t 5.Sair\n");
         scanf("%d", &opcao);
         switch (opcao)
         {
