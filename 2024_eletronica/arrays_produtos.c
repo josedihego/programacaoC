4b5401fe476d54dafc7d039290edc67bb9665157
josedihego@gmail.com <josedihego@gmail.com>
2024-09-11 07:45:38 -0300

    go

--- 2024_eletronica/arrays_produtos.c
+++ 2024_eletronica/arrays_produtos.c
@@ -1,48 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 #define QNT_PROD 5
 #define TAM_NOME_PROD 20
 
-int main(){
+int main()
+{
     int quantidades[QNT_PROD];
-    char produtos [QNT_PROD][TAM_NOME_PROD];
+    char produtos[QNT_PROD][TAM_NOME_PROD];
     int prox_PL = 0;
-    printf("Selecione uma das opções:\n");
-    printf("\t 1. Cadastrar produto\n");
-    printf("\t 2. Excluir produto\n");
-    printf("\t 3. Buscar produto\n");
-    printf("\t 4. Listar produtos\n");
-    printf("\t 5. Sair do sistema\n");
     int opcao = 0;
-    scanf("%d", &opcao);
-    switch (opcao)
+    while (opcao != 5)
     {
-    case 1:
-        printf("Opção inserir selecionada...\\n");
-        printf("Informe o nome do produto:");
-        getchar();
-        fgets(produtos[prox_PL],TAM_NOME_PROD,stdin);
-        printf("Informe a quantidade em estoque:");
-        scanf("%d", &quantidades[prox_PL]);
-        prox_PL = prox_PL+1;
-        break;
-    case 2:
-        printf("Opção excluir selecionada...\\n");
-        break;
-    case 3:
-        printf("Opção buscar selecionada...\\n");
-        break;
-    case 4:
-        printf("Opção listar selecionada...\n");
-        break;
-    case 5:
-        printf("Saindo do sistema...\n");
-        break;    
-    default:
-        printf("Opção inválida\n");
-        break;
+        printf("Selecione uma das opções:\n");
+        printf("\t 1. Cadastrar produto\n");
+        printf("\t 2. Excluir produto\n");
+        printf("\t 3. Buscar produto\n");
+        printf("\t 4. Listar produtos\n");
+        printf("\t 5. Sair do sistema\n");
+
+        scanf("%d", &opcao);
+        switch (opcao)
+        {
+        case 1:
+            printf("Opção inserir selecionada...\\n");
+            printf("Informe o nome do produto:");
+            getchar();
+            fgets(produtos[prox_PL], TAM_NOME_PROD, stdin);
+            printf("Informe a quantidade em estoque:");
+            scanf("%d", &quantidades[prox_PL]);
+            prox_PL = prox_PL + 1;
+            break;
+        case 2:
+            printf("Opção excluir selecionada...\n");
+            break;
+        case 3:
+            printf("Opção buscar selecionada...\n");
+            break;
+        case 4:
+            printf("Opção listar selecionada...\n");
+            for (int i = 0; i < prox_PL; i = i + 1)
+            {
+                printf("Produto n°# %d\n", i + 1);
+                printf("\t Nome: %s", produtos[i]);
+                printf("\t Quantidade: %d", quantidades[i]);
+            }
+            break;
+        case 5:
+            printf("Saindo do sistema...\n");
+            break;
+        default:
+            printf("Opção inválida\n");
+            break;
+        }
     }
 }
