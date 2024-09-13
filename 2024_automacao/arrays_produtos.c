e13d66db704d858e4e2bab7622f5eb72be730b69
josedihego@gmail.com <josedihego@gmail.com>
2024-09-13 08:19:42 -0300

    go

--- 2024_automacao/arrays_produtos.c
+++ 2024_automacao/arrays_produtos.c
@@ -6,44 +6,48 @@
 #define MAX_PROD 10
 #define TAM_MAX_NOME 50
 
-int main(){
+int main()
+{
     char nomes_produtos[MAX_PROD][TAM_MAX_NOME];
-    int qnts_produtos [MAX_PROD];
+    int qnts_produtos[MAX_PROD];
     int proxPL = 0;
-    printf("Informe uma das opções:\n");
-    printf("\t 1. Cadastrar produto\n");
-    printf("\t 2. Remover produto\n");
-    printf("\t 3. Buscar por nome\n");
-    printf("\t 4. Listar produtos\n");
-    printf("\t 5. Sair do sistema\n");
-    int opcao;
-    scanf("%d", &opcao);
-    switch (opcao)
+    int opcao = 0;
+    while (opcao != 5)
     {
+        printf("Informe uma das opções:\n");
+        printf("\t 1. Cadastrar produto\n");
+        printf("\t 2. Remover produto\n");
+        printf("\t 3. Buscar por nome\n");
+        printf("\t 4. Listar produtos\n");
+        printf("\t 5. Sair do sistema\n");
+
+        scanf("%d", &opcao);
+        switch (opcao)
+        {
         case 1:
-        printf("Cadastrar selecionado\n");
-        printf("Informe o nome do produto: ");
-        fgets(nomes_produtos[proxPL], TAM_MAX_NOME, stdin);
-        printf("Informe a quantidade:");
-        scanf("%d", &qnts_produtos[proxPL]);
-        proxPL = proxPL + 1;
-        break;
+            printf("Cadastrar selecionado\n");
+            printf("Informe o nome do produto: ");
+            getchar();
+            fgets(nomes_produtos[proxPL], TAM_MAX_NOME, stdin);
+            printf("Informe a quantidade:");
+            scanf("%d", &qnts_produtos[proxPL]);
+            proxPL = proxPL + 1;
+            break;
         case 2:
-        printf("Remover selecionado\n");
-        break;
+            printf("Remover selecionado\n");
+            break;
         case 3:
-        printf("Buscar selecionado\n");
-        break;
+            printf("Buscar selecionado\n");
+            break;
         case 4:
-        printf("Listar selecionado\n");
-        break;
+            printf("Listar selecionado\n");
+            break;
         case 5:
-        printf("Sair selecionado\n");
-        break;
+            printf("Sair selecionado\n");
+            break;
         default:
-        printf("Opção inválida. Tente novamente.\n");
-        break;
+            printf("Opção inválida. Tente novamente.\n");
+            break;
+        }
     }
-
-
 }
