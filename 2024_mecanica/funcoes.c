55ca7e6874aa3fa9ffe54466e9fb7567cc3d2468
josedihego@gmail.com <josedihego@gmail.com>
2024-10-11 11:28:17 -0300

    go

--- 2024_mecanica/funcoes.c
+++ 2024_mecanica/funcoes.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAX 5
+
 int main(){
-    
+    int valores [MAX];
+    for(int i = 0; i < MAX; i = i +1){
+        printf("Informe o valor de n°%d: ", i+1);
+        scanf("%d", &valores[i]);
+    }
 }
