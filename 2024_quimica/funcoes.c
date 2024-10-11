a4cd14dcd70a604a765f0eba6e113b8bd65642f6
josedihego@gmail.com <josedihego@gmail.com>
2024-10-11 09:22:38 -0300

    go

---
+++ 2024_quimica/funcoes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define TAM 5
+
+int somar_valores(int v [], int q){
+    int soma = 0;
+    for(int i=0; i < q; i = i +1){
+        soma = soma + v[i];
+    }
+    return soma;
+}
+
+int main(){
+    int valores [TAM];
+    for(int i = 0; i < TAM; i = i +1){
+        printf("Informe o valor n° %d: ", i+1);
+        scanf("%d", &valores[i]);
+    }
+    int soma = somar_valores(valores,TAM);
+    printf("Soma dos valores é: %d\n", soma);
+}
