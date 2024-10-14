4769802596a9981742ed8a18a05dc1821f82542e
josedihego@gmail.com <josedihego@gmail.com>
2024-10-14 18:13:18 -0300

    go

--- 2024_INF027/soma_lista_pares.c
+++ 2024_INF027/soma_lista_pares.c
@@ -5,17 +5,23 @@
 #define MAX 4
 int main(){
     int soma = 0;
+    int soma_pares = 0;
+    int qnt_pares = 0;
     int L [MAX];
-    int i;
-    for(i = 0; i < MAX; i = i +1){
+
+    for(int i = 0; i < MAX; i = i +1){
         printf("Informe o %d° valor: ", i+1);
         scanf("%d", &L[i]);
     }
-    printf("Valor de i depois do for: %d\n", i);
-    
+
     for(int i=0; i < MAX; i = i +1){
         soma = soma + L[i];
+        if(L[i]%2==0) {
+            soma_pares = soma_pares + L[i];
+            qnt_pares = qnt_pares +1;
+        }
     }
-    printf("A soma dos %d valores digitados é: %d", MAX, soma);
+    printf("A soma dos %d valores digitados é: %d\n", MAX, soma);
+    printf("Existem %d números pares e sua soma é %d\n",qnt_pares,soma_pares);
     return EXIT_SUCCESS;
 }
