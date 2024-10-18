82db9b299c16f6bdbb390dfc10e5c3d352ad7c85
josedihego@gmail.com <josedihego@gmail.com>
2024-10-18 08:32:52 -0300

    go

--- 2024_automacao/sensor.c
+++ 2024_automacao/sensor.c
@@ -23,9 +23,22 @@ int main(){
                 printf("Informe a temperatura para cadastro:");
                 scanf("%d",&medidas[posicao]);
                 posicao = (posicao + 1) % 1000;
+                break;
             case 2:
-
+                float media;
+                float soma = 0.0;
+                int qnt = 0;
+                for(int i = 0 ; i < 1000; i = i +1){
+                    if(medidas[i]!= ZERO_ABS){
+                        soma = soma + medidas[i];
+                        qnt = qnt +1;
+                    }
+                }
+                media = soma/qnt;
+                printf("Média = %.2f\n", media);
+                break;
             case 3:
+            break;
         }
     }
 }
