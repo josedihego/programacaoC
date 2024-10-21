efdce7e65fe6d28f200956ad3de05ab626c751ee
josedihego@gmail.com <josedihego@gmail.com>
2024-10-21 18:33:25 -0300

    go

---
+++ T/UN2_c4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  int valor;
+  while(3 > 2){
+    printf("Informe um valor:");
+    scanf("%d",&valor);
+  }
+}
---
+++ T/UN2_c5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int lista [100];
+  for(int i=0; i < 100; i = i + 10){
+    scanf("%d",&lista[i]);
+  }
+}
