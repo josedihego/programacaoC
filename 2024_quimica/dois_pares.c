48c47203254b30c0402f8ee7d04e23bdfc71766f
josedihego@gmail.com <josedihego@gmail.com>
2024-11-11 17:33:51 -0300

    go

---
+++ 2024_quimica/dois_pares.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int qnt = 0;
+    int valor;
+    while(qnt < 2){
+        printf("Informe um valor: ");
+        scanf("%d", &valor);
+        if(valor%2==0)  qnt = qnt +1;
+        else qnt = 0;
+    }
+    printf("Dois pares seguidos foram digitados\n");
+}
