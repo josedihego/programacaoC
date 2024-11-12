fbab7593b53a340acb49794c4bdda42fa8f55091
josedihego@gmail.com <josedihego@gmail.com>
2024-11-12 19:45:24 -0300

    go

---
+++ 2024_INF027/caminhada_robotica.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float calcular_distancia(float x1, float y1, float x2, float y2)
+{
+    float d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    return d;
+}
+int main()
+{
+    float x1 = 0;
+    float y1 = 0;
+
+    float x2;
+    float y2;
+    printf("Informe a coordenada  x do próximo ponto:");
+    scanf("%f", &x2);
+    printf("Informe a coordenada  y do próximo ponto:");
+    scanf("%f", &y2);
+    float distancia_total = 0;
+    while (x2 != 0 && y2 != 0)
+    {
+        float d = calcular_distancia(x1, y1, x2, y2);
+        distancia_total = distancia_total + d;
+        x1 = x2;
+        y1 = y2;
+        printf("Informe a coordenada  x do próximo ponto:");
+        scanf("%f", &x2);
+        printf("Informe a coordenada  y do próximo ponto:");
+        scanf("%f", &y2);
+    }
+    printf("Fim do percurso. Distância percorrida: %.2f", distancia_total);
+}
