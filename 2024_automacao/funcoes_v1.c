53d2ffcc33f7df36becaffb59509af2ca99b79de
josedihego@gmail.com <josedihego@gmail.com>
2024-11-29 08:52:37 -0300

    GO

--- 2024_automacao/funcoes_v1.c
+++ 2024_automacao/funcoes_v1.c
@@ -10,12 +10,15 @@ float somar(float valores [], int tamanho){
 }
 
 float calc_media(float valores[], int tamanho){
-    
+    float media = somar(valores,tamanho)/tamanho;
+    return media;
 }
 
 int main(){
     float valores [] = {78, 67.5, 92, 108, -3, 4, 9, 25,-50.8,120.4};
     float res = somar(valores,10);
+    float media = calc_media(valores,10);
     printf("Soma= %.2f\n", res);
+    printf("Média = %.2f\n",media);
 
 }
--- 2024_quimica/funcoes_v1.c
+++ 2024_quimica/funcoes_v1.c
@@ -26,7 +26,7 @@ float maximo(float valores[], int tamanho){
 }
 
 int main(){
-    float valores [] = {198,-30,45.6,67.9,90.5,81,-5,43.5,82.7};
+    float valores [] = {198,-   30,45.6,67.9,90.5,81,-5,43.5,82.7,-32.8};
     float res = somar(valores,9);
     printf("Soma = %.2f\n", res);
     float media = mediar(valores,9);
