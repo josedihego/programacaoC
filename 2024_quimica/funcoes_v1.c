f7d30a037d5b3246384c0785dd7cc893e3b4159c
josedihego@gmail.com <josedihego@gmail.com>
2024-11-29 10:12:07 -0300

    go

--- 2024_quimica/funcoes_v1.c
+++ 2024_quimica/funcoes_v1.c
@@ -27,6 +27,7 @@ float maximo(float valores[], int tamanho){
 
 int main(){
     float valores [] = {198,-30,45.6,67.9,90.5,81,-5,43.5,82.7,-32.8};
+    char maquinas [10] [5] ={"M1","M3","M2","M2","M1","M3","M4","M1","M1","M2"};
     float res = somar(valores,10);
     printf("Soma = %.2f\n", res);
     float media = mediar(valores,10);
