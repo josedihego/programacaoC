e2bfe3ba1463f75b4d0c1704b4986c1c4acce5c3
josedihego@gmail.com <josedihego@gmail.com>
2024-11-29 07:28:02 -0300

    GO

--- 2024_automacao/funcoes_v1.c
+++ 2024_automacao/funcoes_v1.c
@@ -14,7 +14,7 @@ float calc_media(float valores[], int tamanho){
 }
 
 int main(){
-    float valores [] = {78.00, 67.50, 92.00, 108.00, -3.00, 4.00, 9.00};
+    float valores [] = {78, 67.5, 92, 108, -3, 4, 9, 25,-50.8,120.4};
     float res = somar(valores,7);
     printf("Soma= %.2f\n", res);
 
