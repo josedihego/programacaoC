b097e44d9adbe43645cc4c1143ab5a93902be820
josedihego@gmail.com <josedihego@gmail.com>
2024-12-06 07:58:24 -0300

    go

--- 2024_automacao/funcoes_v1.c
+++ 2024_automacao/funcoes_v1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 float somar(float valores [], int tamanho){
     float soma = 0;
@@ -14,8 +15,20 @@ float calc_media(float valores[], int tamanho){
     return media;
 }
 
+int contar_ocorrencias(char maquinas [][3], char maq_bus [3], int qnt_maquinas){
+     int ocorrencias = 0;
+     for(int i = 0; i < qnt_maquinas; i= i +1){
+        if(strcmp(maquinas[i], maq_bus)==0){
+            ocorrencias =  ocorrencias +1;
+        }
+     }
+     return ocorrencias;
+}
+
 int main(){
     float valores [] = {78, 67.5, 92, 108, -3, 4, 9, 25,-50.8,120.4};
+    char maquinas [10][3] = {"M1","M3","M4","M3","M1","M1","M2","M1","M3","M3"};
+
     float res = somar(valores,10);
     float media = calc_media(valores,10);
     printf("Soma= %.2f\n", res);
