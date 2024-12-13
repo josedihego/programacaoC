31291723e093e1e051abccef2e55b9e106b0589b
josedihego@gmail.com <josedihego@gmail.com>
2024-12-13 12:07:26 -0300

    go

--- 2024_mecanica/funcoes_V1.c
+++ 2024_mecanica/funcoes_V1.c
@@ -28,6 +28,18 @@ int contar_ocorrencias(char maquinas[][3], char maq_bus[], int qnt){
     return ocorrencias;
 }
 
+float media_por_maquina(char nome [], char maquinas [][3], float valores [], int quantidade){
+    int ocorrencias = 0;
+    float soma = 0.0;
+    for(int i =0; i < quantidade; i = i +1){
+        if (strcmp(nome, maquinas[i])==0){
+            soma = soma + valores[i];
+            ocorrencias = ocorrencias +1;
+        }
+    }
+    return soma/ocorrencias;
+}
+
 int main(){
     float valores [10] ={56.6,67.4,12.4,-34.5,10,25,12,9,-3,30};
     char maquinas [10][3]= {"M2","M2","M3","M1","M1","M3","M3","M1","M4","M1"};
@@ -39,5 +51,7 @@ int main(){
     printf("Média = %.2f\n",med);
     printf("Número ocorrências M1: %d\n",oc_M1);
     printf("Número ocorrências M2: %d\n", oc_M2);
+    float media_m1 = media_por_maquina("M1", maquinas, valores, 10);
+    printf("Média para M1 = %.3f\nx", media_m1);
 }
 
