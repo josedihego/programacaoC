16455b9ac8ac5bea0bc5cbf2a04a340d0e58bb54
josedihego@gmail.com <josedihego@gmail.com>
2024-12-17 20:00:05 -0300

    to

---
+++ 2024_INF027/for_3_aninhado.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+
+x=2, y=2, z=2x=2, y=2, z=2int calc_raiz(int x, int y, int z){
+    int res = 3 * (x*x)+ 2 *(y*y*y) - 7*(z*z);
+    return res;
+}
+int main(){
+   
+    for(int x=0; x <= MAX ; x = x+1){
+        for(int y=0; y <= MAX; y = y+1){
+            for(int z=0; z <= MAX; z = z+1){
+                if (calc_raiz(x,y,z)==0){
+                    printf("achei para x=%d, y=%d, z=%d\n",x,y,z);
+                }
+            }
+        }
+    }
+}
