a6ae8fa5a5fcf65b5afa5fac2e0554a5b13a6af5
josedihego@gmail.com <josedihego@gmail.com>
2025-01-27 17:57:35 -0300

    go

---
+++ 2024_INF027/bubble_sort_ordenacao.c
@@ -0,0 +1,22 @@
+
+#define TAM 11
+
+int main(){
+ 
+    int  valores [TAM] = {5,13,7,5,26,13, -1, 10, 9, -3,0};
+    for(int i = 0; i < TAM ; i = i +1){
+        for(int j = TAM-1; j > i; j = j-1){
+            if(valores[j] < valores[j-1]){
+                int temp = valores[j];
+                valores[j] = valores[j-1];
+                valores[j-1] = temp;
+            }
+        }
+    }
+    printf("[");
+    for(int i=0; i <TAM ; i = i +1){
+        printf("%d, ", valores[i]);
+    }
+    printf("]\n");
+
+}
