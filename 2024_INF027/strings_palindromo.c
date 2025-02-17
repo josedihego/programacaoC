9c20e9775203efc21d80cb7f0ef3d65771f5ff39
josedihego@gmail.com <josedihego@gmail.com>
2025-02-17 19:08:37 -0300

    go

---
+++ 2024_INF027/strings_palindromo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char texto [100];
+    char invertido[100];
+    printf("Informe um texto para checar se palíndromo: ");
+    fgets(texto,100,stdin);
+}
