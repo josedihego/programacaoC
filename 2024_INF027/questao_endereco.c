befbd1c4721143c930155942996878e824e368e8
josedihego@gmail.com <josedihego@gmail.com>
2025-03-31 16:27:35 -0300

    go

---
+++ 2024_INF027/questao_endereco.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    char endereco [200];
+    printf("Informe seu endereço completo:");
+    fgets(endereco, 200,stdin);
+    int i = 0;
+    while(endereco[i] != '\0'){
+        if(endereco[i])
+    }
+}
