87fe447897fb89aa991de55200e712e1166349f9
josedihego@gmail.com <josedihego@gmail.com>
2025-03-31 18:34:03 -0300

    go

--- 2025_INF027/maior_entre_tres.c
+++ 2025_INF027/maior_entre_tres.c
@@ -1,22 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n1,n2,n3;
+int main()
+{
+    int n1, n2, n3;
     printf("Informe o valor de n1:");
-    scanf("%d",&n1);
+    scanf("%d", &n1);
     printf("Informe o valor de n2:");
-    scanf("%d",&n2);
+    scanf("%d", &n2);
     printf("Informe o valor de n3:");
-    scanf("%d",&n3);
-    if(n1>n2 && n1>n3){
-        printf("n1=%d é o maior\n",n1);
+    scanf("%d", &n3);
+    if (n1 == n2 && n2 == n3)
+    {
+        printf("Os valores são iguais a %d\n", n1);
     }
-    else{
-        if(n2>n3){
-            printf("n2=%d é o maior\n",n2);
+    else
+    {
+        if (n1 > n2 && n1 > n3)
+        {
+            printf("n1=%d é o maior\n", n1);
         }
-        else{
-            printf("n3=%d é o maior\n",n3);
+        else
+        {
+            if (n2 > n3)
+            {
+                printf("n2=%d é o maior\n", n2);
+            }
+            else
+            {
+                printf("n3=%d é o maior\n", n3);
+            }
         }
     }
 }
