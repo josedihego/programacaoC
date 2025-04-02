b89dc78ce9285e454baba099894baf09f01601a1
josedihego@gmail.com <josedihego@gmail.com>
2025-04-02 14:49:07 -0300

    go

---
+++ 2025_edificacoes_1811/pitagoras.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float c1, c2, h;
+    printf("Informe o valor do cateto 1:");
+    scanf("%f",&c1);
+    printf("Informe o valor do cateto 2:");
+    scanf("%f",&c2);
+    h = sqrt(c1*c1 + c2*c2);
+    printf("Hipotenusa = %.2f\n",h);
+
+}
