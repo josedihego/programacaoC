b37e0b14d26564d7e66bdc65f74e0fb7df633908
josedihego@gmail.com <josedihego@gmail.com>
2025-05-05 16:49:32 -0300

    go

---
+++ 2025_edificacoes_1813/cod_04_soma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int soma = 0;
+    for(int i = 0; i < 10; i = i + 1){
+        soma = soma + i;
+    }
+    printf("Soma = %d\n", soma);
+}
