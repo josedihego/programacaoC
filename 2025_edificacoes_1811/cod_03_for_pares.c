07e45db1c624ef185b2788ae7f423f4ba524d92f
josedihego@gmail.com <josedihego@gmail.com>
2025-05-09 10:34:30 -0300

    go

---
+++ 2025_edificacoes_1811/cod_03_for_pares.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    for(int i = 0; i < 10; i = i +1){
+        printf("par  = %d\n",i * 2);
+    }
+}
---
+++ 2025_edificacoes_1811/cod_04_for_impares.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    for(int i = 0; i < 100; i = i +1){
+        printf("impar = %d\n", (i * 2) + 1);
+    }
+}
