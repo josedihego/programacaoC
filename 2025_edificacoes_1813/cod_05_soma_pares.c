54e92ad36718fcac373f9cc3a4c074f0c75d1002
josedihego@gmail.com <josedihego@gmail.com>
2025-05-09 10:51:55 -0300

    go

---
+++ 2025_edificacoes_1813/cod_05_soma_pares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Objetivo: fazer a soma dos 100 primeiros números pares
+0 + 2 + 4 + ... + 198
+*/
+int main(){
+    // definição de variáveis
+    int somatorio = 0;
+    for(int i = 0; i < 100; i = i+1){
+        somatorio = somatorio + (i * 2);
+    }
+    printf("Somatório = %d\n", somatorio);
+}
