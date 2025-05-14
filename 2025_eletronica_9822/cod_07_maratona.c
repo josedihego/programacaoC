c2223c07b624f81522ac5ca5c58f4a799a952cbb
josedihego@gmail.com <josedihego@gmail.com>
2025-05-14 10:49:38 -0300

    go

--- 2025_eletronica_9822/cod_07_maratona.c
+++ 2025_eletronica_9822/cod_07_maratona.c
@@ -6,10 +6,15 @@ int main(){
     int dm = 42195;// distância maratona
     int f = 200;// distância de incremento
     int d;
+    int distancia_total = 0;
     for(d = 0; di < dm; d = d+1){
+        distancia_total = distancia_total+ di;
         di = di + f;
+        printf("%d\n",di);
     }
+    distancia_total = distancia_total+ di;
     printf("Depois de %d dias o corredor corre uma maratona\n",d);
     printf("No dia %d ele percorre %d m 🏃 \n",d+1,di);
+    printf("Distância percorrida depois de %d dias é: %.2f km\n",d, distancia_total/1000.0);
 
 }
