94add9a5d3954077600b4ea1577bb28219d996a7
josedihego@gmail.com <josedihego@gmail.com>
2025-05-19 16:52:44 -0300

    go

--- 2025_edificacoes_1813/cod_07_corrida_maratona.c
+++ 2025_edificacoes_1813/cod_07_corrida_maratona.c
@@ -6,10 +6,14 @@ int main(){
     int nd;
     int distancia = 5000;
     int incremento = 200;
-
+    int somatorio = 5000;
     for(nd=1; distancia <= maratona; nd = nd+1){
         distancia = distancia+200;
+        somatorio = somatorio + distancia;
+
     }
     printf("🥇 Diva conseguirá correr a maratona no dia n° %d\n",nd);
     printf("📍 Nesse dia Diva terá corrido %d\n", distancia);
+    printf("Diva percorreu ao longo de todo o treinamento %d metros\n", somatorio);
+    printf("Ou seja %.2f km\n", somatorio/1000.0);
 }
