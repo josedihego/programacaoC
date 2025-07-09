fadbd694b6048eae2dc530a0d0bbdc247a1669ab
josedihego@gmail.com <josedihego@gmail.com>
2025-07-09 12:10:25 -0300

    go

--- 2025_automacao_5821/cod_10_madereira.c
+++ 2025_automacao_5821/cod_10_madereira.c
@@ -16,8 +16,18 @@ int main(){
     scanf("%f",&pkg);
 
     // calculo do ganho com cortes de 0.5 m
-    
+    int qnt_pedacos = 0;
+    float sobra = C;
+    for(sobra = C; sobra >= 0.5; sobra = sobra - 0.5){
+        qnt_pedacos = qnt_pedacos +1;
+    }
+    float volume_sobra = sobra * L * A;
+    float massa_sobra = volume_sobra *  D;
+    float valor_ganho = (qnt_pedacos * p5) + (massa_sobra*pkg);
 
+    printf("Quantidade pedaços .5 m: %d\n", qnt_pedacos);
+    printf("Quantidade de kg de pó de madeira: %.2f\n",massa_sobra);
+    printf("Valor total arrecadado: %.2f R$\n",valor_ganho);
     // calculo do ganho com cortes de 1.5 m
 
     // calculo do ganho com cortes de 2.2 m
