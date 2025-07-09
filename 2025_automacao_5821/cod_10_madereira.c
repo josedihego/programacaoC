ce6fa08c99bfe4e159d48c7cd9c0f6eff7d5bfb8
josedihego@gmail.com <josedihego@gmail.com>
2025-07-09 11:51:44 -0300

    go

---
+++ 2025_automacao_5821/cod_10_madereira.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float C, L, A;
+    float p5, p15, p22;
+    float D;
+    float pkg;
+    printf("Informe o comprimento, largura e altura:");
+    scanf("%f %f %f", &C, &L, &A);
+    printf("Informe os preços .5 m, 1.5 m e 2.2 m:");
+    scanf("%f %f %f", &p5, &p15, &p22);
+    printf("Informe a densidade:");
+    scanf("%f",&D);
+    printf("Informe o preço por kg:");
+    scanf("%f",&pkg);
+
+    // calculo do ganho com cortes de 0.5 m
+    
+
+    // calculo do ganho com cortes de 1.5 m
+
+    // calculo do ganho com cortes de 2.2 m
+
+    // decidir e mostrar qual o melhor esquema de corte
+    // se com 0.5 m, 1.5 m ou 2.2 m
+
+}
