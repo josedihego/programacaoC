e5ba8f9385c54ab7e7947479a45960aeba16fa9b
josedihego@gmail.com <josedihego@gmail.com>
2025-07-14 16:53:07 -0300

    go

--- 2025_edificacoes_1813/cod_08_randomicos.c
+++ 2025_edificacoes_1813/cod_08_randomicos.c
@@ -4,9 +4,21 @@
 
 int main(){
     srand(time(NULL));
-    int dado = rand()%6;
-    dado = dado +1;
-    printf("Dado é %d\n",dado);
+    int dado1 = rand()%6;
+    dado1 = dado1 +1;
+    int dado2 = rand()%6;
+    dado2 = dado2+1;
+    int resposta;
+    printf("Informe o valor do dado1 + dado2: ");
+    scanf("%d",&resposta);
+    if(resposta == dado1+dado2){
+        printf("Você acertou! Parabéns!\n");
+    }else{
+        printf("Você errou! Tente novamente!\n");
+    }
+    printf("Dado 1 é:  %d\n",dado1);
+    printf("Dado 2 é: %d\n",dado2);
+
     //int agora = time(NULL);
     //printf("Agora : %d\n", agora);
 }
