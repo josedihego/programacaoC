29f462012f74aec730ce542ef743e40130a361ac
josedihego@gmail.com <josedihego@gmail.com>
2025-07-28 18:38:10 -0300

    go

--- 2025_INF027/questao_01_2024_2_max_pooling.c
+++ 2025_INF027/questao_01_2024_2_max_pooling.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 
 #define T 6
-#define P 2
+#define P 6
+#define TR T/P
+
 int main(){
+  
+  /*
   int matrix [T][T];  
   for(int l = 0; l < T; l = l +1){
     for(int c =0; c < T; c = c+1){
-        printf("Informe a posição da matrix[%d][%d]:", l, c);
-        scanf("%d", &matrix[l][c]);
+       printf("Informe a posição da matrix[%d][%d]:", l, c);
+       scanf("%d", &matrix[l][c]);
+
     }
   }
+    */
 
-  int matrix[6][6] = {
+  int matrix[T][T] = {
         { 3,  7,  1, 11,  5,  9},
         {12,  2,  8,  4, 10,  6},
         { 5,  9,  3,  7,  1, 11},
@@ -20,4 +26,30 @@ int main(){
         { 1, 11,  5,  9,  3,  7},
         { 8,  4, 10,  6, 12,  2}
     };
+
+    int resultado[TR][TR];
+
+    for(int l=0; l < T; l = l + P){
+        printf("l=%d\n",l);
+        for(int c =0; c < T; c = c+P){
+            printf("\t c= %d, ", c);
+            int max = matrix[l][c];
+            for(int i = l; i < l+P; i = i +1){
+                for(int j = c; j < c+P; j = j +1){
+                    if(matrix[i][j] > max){
+                        max = matrix[i][j];
+                    }
+                }
+            }
+            printf(" max = %d\n", max);
+            resultado[l/P][c/P] = max;
+        }
+    }
+    printf("Resultado : \n");
+    for(int l = 0 ; l < TR; l = l +1){
+        for(int c =0; c < TR; c = c+1){
+            printf("%d ", resultado[l][c]);
+        }
+        printf("\n");
+    }
 }
