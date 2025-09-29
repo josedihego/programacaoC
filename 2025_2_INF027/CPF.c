a9bf6b477452ccf0d4c592128516bf955d54806f
josedihego@gmail.com <josedihego@gmail.com>
2025-09-29 18:40:24 -0300

    go

---
+++ 2025_2_INF027/CPF.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int c1,c2,c3,c4,c5,c6,c7,c8,c9;
+    int d1,d2;
+    printf("Informe seu CPF:");
+    scanf("%d%d%d.%d%d%d.%d%d%d-%d%d",&c1,&c2,&c3,&c4,&c5,&c6,&c7,&c8,&c9,&d1,&d2);
+    int soma = (c1*10)+(c2*9)+(c3*8)+(c4*7)+(c5*6)+(c6*5)+(c7*4)+(c8*3)+(c9*2);
+    int resto = soma % 11;
+    int div1, div2;
+    if(resto<2){
+        div1 = 0;
+    }
+    else{
+        div1 = 11 - resto;
+    }
+    if(d1==div1){
+        soma = (c1*11)+(c2*10)+(c3*9)+(c4*8)+(c5*7)+(c6*6)+(c7*5)+(c8*4)+(c9*3)+(d1*2);
+        resto = soma % 11;
+        if(resto < 2){
+            div2 = 0;
+        }
+        else{
+            div2 = 11 - resto;
+        }
+        if(d2==div2){
+            printf("CPF válido ✅ \n");
+        }
+        else{
+            printf("CPF inválido no DV2 ❌ \n");
+        }
+    }
+    else{
+        printf("CPF inválido no DV1 ❌ \n");
+    }
+}
--- 2025_2_INF027/bhaskara.c
+++ 2025_2_INF027/bhaskara.c
@@ -4,9 +4,22 @@
 
 
 int main(){
-    float valor ;
-    printf("Informe um valor: ");
-    scanf("%f", &valor);
-    float resultado = sqrt(valor);
-    printf("Raiz quadrada de %.2f é %.4f\n",valor,resultado);
+   float a, b, c;
+   printf("Informe a: ");
+   scanf("%f",&a);
+   printf("Informe b: ");
+   scanf("%f",&b);
+   printf("Informe c: ");
+   scanf("%f",&c);
+   float delta = (b*b) - (4*a*c);
+
+   if(delta>=0){
+        float x1 = (-b + sqrt(delta))/ (2*a);
+        float x2 = (-b - sqrt(delta))/(2*a);
+        printf("Raiz 1 é %.2f\n", x1);
+        printf("Raiz 2 é %.2f\n", x2);
+   }
+   else{
+    printf("Equação não tem raizes reais!\n");
+   }
 }
