fa1a2ce63823d488507fd1c316dac732ea0e9685
josedihego@gmail.com <josedihego@gmail.com>
2025-10-29 09:39:33 -0300

    go

---
+++ 2025_eletronica_9822/cod_17_ponteiros_arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int calc_pares(int valores [], int tam){
+    int qnt = 0;
+    for(int i = 0; i < tam; i = i +1){
+        if(valores[i]%2==0){
+            qnt = qnt +1;
+        }
+    }
+    return qnt;
+}
+
+int main(){
+    srand(time(NULL));
+    int * numeros;
+    numeros = malloc(10 * sizeof(int));
+    printf("[ ");
+    for(int i = 0; i < 10; i = i +1){
+        numeros[i] = rand() % 100;
+        printf("%d, ", numeros[i]);
+    }
+    printf(" ]");
+    int qnt = calc_pares(numeros, 10);
+    printf("Destes %d são pares\n",qnt);
+}
