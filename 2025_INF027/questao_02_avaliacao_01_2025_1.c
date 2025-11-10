16842a636c53285501f9cb3d79b9c6ac1995c73b
josedihego@gmail.com <josedihego@gmail.com>
2025-11-10 18:43:12 -0300

    go

---
+++ 2025_INF027/questao_02_avaliacao_01_2025_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int M, P, D;
+    printf("Informe os valores de M (tamanho da avenida), P(distância entre pontos ônibus) e D (distância da escola):");
+    printf("\tM = ");
+    scanf("%d",&M);
+    printf("\tP = ");
+    scanf("%d",&P);
+    printf("\tD = ");
+    scanf("%d",&D);
+    int n_p1 = D/P + 1;
+    int n_p2 = n_p1+1;
+
+}
