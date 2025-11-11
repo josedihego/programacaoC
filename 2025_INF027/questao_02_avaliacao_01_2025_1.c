f5647aea9adf975e76f4452a8744c86d9a8ffb83
josedihego@gmail.com <josedihego@gmail.com>
2025-11-11 19:21:15 -0300

    go

--- 2025_INF027/questao_02_avaliacao_01_2025_1.c
+++ 2025_INF027/questao_02_avaliacao_01_2025_1.c
@@ -4,7 +4,7 @@
 
 int main(){
     int M, P, D;
-    printf("Informe os valores de M (tamanho da avenida), P(distância entre pontos ônibus) e D (distância da escola):");
+    printf("Informe os valores de M (tamanho da avenida), P(distância entre pontos ônibus) e D (distância da escola):\n");
     printf("\tM = ");
     scanf("%d",&M);
     printf("\tP = ");
@@ -13,5 +13,17 @@ int main(){
     scanf("%d",&D);
     int n_p1 = D/P + 1;
     int n_p2 = n_p1+1;
+    printf("Paradas para escolher: essa %d ou essa %d\n",n_p1, n_p2);
+
+    int caminhada1 = (n_p1-1) * P;
+    int caminhada2 = (n_p2-1) * P;
+
+    if(D-caminhada1 < caminhada2-D){
+        printf("Caminhe do início da avenida para o final da avenida %d metros e desça no ponto %d\n",D-caminhada1, n_p1);
+    }
+    else{
+        printf("Caminhe do final da avenida para o ínicio da avenida %d metros e desça no ponto %d\n", caminhada2-D, n_p2);
+    }
+
 
 }
