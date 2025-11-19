85ba74858bf393194e4517ab8518cccb513b981f
josedihego@gmail.com <josedihego@gmail.com>
2025-11-19 09:39:17 -0300

    go

--- 2025_eletronica_9822/cod_17_ponteiros_arrays.c
+++ 2025_eletronica_9822/cod_17_ponteiros_arrays.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+#define TAM 10
 
 int calc_pares(int valores [], int tam){
     int qnt = 0;
@@ -12,24 +14,50 @@ int calc_pares(int valores [], int tam){
     return qnt;
 }
 
-void ordenar(int valores [], int tam){
-
+void ordenar(int valores [], int tam, bool crescente){
+    for(int i =0; i < tam; i = i +1){
+        for(int j = i; j < tam; j = j+1){
+            
+            if(crescente==false && valores[i] < valores[j]){
+                int temp = valores[i];
+                valores[i] = valores[j];
+                valores[j] = temp; 
+            }
+            if(crescente==true && valores[i] > valores[j]){
+                int temp = valores[i];
+                valores[i] = valores[j];
+                valores[j] = temp; 
+            }
+        }
+    }
+}
+void imprimir_array(int valores [], int tam){
+    printf("\n[");
+    for(int i = 0; i < tam; i = i +1){
+        printf("%d, ", valores[i]);
+    }
+    printf("] \n");
 }
 
 // crie uma função que recebe dois arrays,
 // informa qual deles apresenta a maior soma de valores
 // e ao final imprime todos os valores dos dois arrays em ordem
 
+
+
 int main(){
     srand(time(NULL));
     int * numeros;
-    numeros = malloc(10 * sizeof(int));
-    printf("[ ");
-    for(int i = 0; i < 10; i = i +1){
+    numeros = malloc(TAM * sizeof(int));
+    for(int i = 0; i < TAM; i = i +1){
         numeros[i] = rand() % 100;
-        printf("%d, ", numeros[i]);
     }
-    printf(" ]");
-    int qnt = calc_pares(numeros, 10);
+    imprimir_array(numeros,TAM);
+    int qnt = calc_pares(numeros, TAM);
     printf("Destes %d são pares\n",qnt);
+    ordenar(numeros,TAM, false);
+    imprimir_array(numeros, TAM);
+    ordenar(numeros, TAM, true);
+    imprimir_array(numeros,TAM);
+
 }
