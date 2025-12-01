77009499e2c8bfda11d5e593a47d82605b77f004
josedihego@gmail.com <josedihego@gmail.com>
2025-12-01 16:32:53 -0300

    go

--- 2025_edificacoes_1813/cod_12_funcoes.c
+++ 2025_edificacoes_1813/cod_12_funcoes.c
@@ -9,8 +9,18 @@ int soma(int valores [], int tamanho){
     return res;
 }
 
+int produto(int valores[], int tamanho){
+    int res = 1;
+    for(int p = 0; p < tamanho ; p = p+1){
+        res = res * valores[p];
+    }
+    return res;
+}
+
 int main(){
-   int lista [5] ={5,89,900,-76,4560};
+   int lista [5] ={5,89,90,-76,45};
    int r = soma(lista,5);
    printf("Resultado = %d\n",r);
+   int rp = produto(lista,5);
+   printf("Resultado produto = %.d\n", rp);
 }
