5429a1ea8e880946d56d74ede9eefc979ad773a7
Jose Dihego <josedihego@gmail.com>
2026-01-13 19:44:40 -0300

    go

--- 2025_2_INF027/avaliacao_3_q_2_prova_2014_2.c
+++ 2025_2_INF027/avaliacao_3_q_2_prova_2014_2.c
@@ -5,35 +5,53 @@
 int main(){
     char texto [256];
     char consoantes [] = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
-    printf("Informe um texto");
+    printf("Informe um texto:");
     fgets(texto,256, stdin);
-
-    int p = 0;
+  
+    int p_t = 0;
     int qnt = 0;
-    while(texto[p] != '\0'){
-        int c1 = texto[p];
-        int pi = 0;
+    while(texto[p_t] != '\0'){
+        int c1 = texto[p_t];
+        int p_c = 0;
         bool c1_consoante = false;
-        while(consoantes[pi]!= '\0' && c1_consoante!=true){
-            if(consoantes[pi]==c1){
+        while(consoantes[p_c]!= '\0' && c1_consoante!=true){
+            if(consoantes[p_c]==c1){
                 c1_consoante = true;
             }
-            pi = pi+1;
+            p_c = p_c+1;
         }
         if(c1_consoante==true){
-            int c2 = texto[p+1];
-            pi = 0;
+            int c2 = texto[p_t+1];
+            p_c = 0;
             bool c2_consoante =false;
-            while(consoantes[pi]!= '\0' && c2_consoante !=true){
-                if(consoantes[pi] == c2){
+            while(consoantes[p_c]!= '\0' && c2_consoante !=true){
+                if(consoantes[p_c] == c2){
                     c2_consoante = true;
                 }
-                pi = pi+1;
+                p_c = p_c+1;
             }
             if(c2_consoante ==true){
                 qnt = qnt+1;
+                p_t = p_t +1;
+                char prox = texto[p_t];
+                bool prox_e_cons = true;
+                while(prox_e_cons==true){
+                    p_t = p_t + 1;
+                    prox = texto[p_t];
+                    p_c = 0;
+                    prox_e_cons = false;
+                    while(consoantes[p_c] != '\0'){
+                        if(prox == consoantes[p_c]){
+                            prox_e_cons = true;
+                        }
+                        p_c = p_c+1;
+                    }
+
+                }
+
             }
         }
-        p = p +1;
+        p_t = p_t +1;
     }
+    printf("Quantidade de encontro consonantal: %d\n", qnt);
 }
