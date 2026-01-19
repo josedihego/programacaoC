01154e62b084a5a8e84fe72afaca2e615068c1a0
Jose Dihego <josedihego@gmail.com>
2026-01-19 18:34:59 -0300

    go

--- 2025_2_INF027/avaliacao_3_q_1_ano_2025_1.c
+++ 2025_2_INF027/avaliacao_3_q_1_ano_2025_1.c
@@ -1,46 +1,55 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 #define T 100
-int main(){
+int main()
+{
 
     bool eh_cobol = true;
-    char texto [T];
-    char cobol[5]="cobol";
+    char texto[T];
+    char cobol[5] = "cobol";
     printf("Informe o texto: ");
     fgets(texto, T, stdin);
     printf("%s", texto);
-     // cap-one-best-opinion-language
+    // cap-one-best-opinion-language
     int i = 0;
     int qnt_hifens = 0;
     int tamanho_palavra = 0;
-    while(texto[i] != '\0' && eh_cobol){
-        char c  =texto[i];
-        if(c!='-'){
-            tamanho_palavra = tamanho_palavra+1;
+    while (texto[i] != '\0' && eh_cobol)
+    {
+        char c = texto[i];
+        if (c != '-')
+        {
+            tamanho_palavra = tamanho_palavra + 1;
         }
-        else{
-            if(cobol[qnt_hifens]!= texto[i-1] && cobol[qnt_hifens]!= texto[i-tamanho_palavra]){
+        else
+        {
+            if (cobol[qnt_hifens] != texto[i - 1] && cobol[qnt_hifens] != texto[i - tamanho_palavra])
+            {
                 eh_cobol = false;
             }
             tamanho_palavra = 0;
             qnt_hifens = qnt_hifens + 1;
         }
 
-        i = i +1;
-        if(texto[i]=='\0'){
-        
-            if(cobol[qnt_hifens]!= texto[i-1] &&
-            cobol[qnt_hifens] != texto [i-tamanho_palavra]){
+        i = i + 1;
+        if (texto[i] == '\0')
+        {
+
+            if (cobol[qnt_hifens] != texto[i - 1] &&
+                cobol[qnt_hifens] != texto[i - tamanho_palavra])
+            {
                 eh_cobol = false;
             }
         }
     }
-    if(eh_cobol){
+    if (eh_cobol)
+    {
         printf("COBOL\n");
     }
-    else{
+    else
+    {
         printf("BUG\n");
     }
 
