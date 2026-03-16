ddf199c74652e18b14da815ce3e8acf42aac4f30
Jose Dihego <josedihego@gmail.com>
2026-03-16 18:45:53 -0300

    go

---
+++ 2026_1_INF027/cod_03_pitagoras.c
@@ -0,0 +1,9 @@
+
+/**
+ * Crie um programa que leia 3 valores do tipo
+ * inteiro e informa se estes três valores podem
+ * formar um triângulo retângulo.
+ * Para isso saiba que um triângulo retangulo satsifaz
+ * hiotenusa^2 = cateto1^2 + cateto_2^2
+ * 
+ */
