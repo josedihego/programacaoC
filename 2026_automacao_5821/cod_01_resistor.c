5a763093434b27736d3c47b938ed1441fd54775f
Jose Dihego <josedihego@gmail.com>
2026-03-18 13:32:24 -0300

    go

--- 2026_automacao_5821/cod_01_resistor.c
+++ 2026_automacao_5821/cod_01_resistor.c
@@ -1,1 +1,14 @@
-// olá pessoal
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    float R1;
+    float R2;
+    float RF;
+    int opcao;
+    printf("Informe a resistência número 1: ");
+    scanf("%f",&R1);
+    printf("Informe a resistência número 2: ");
+    scanf("")
+}
