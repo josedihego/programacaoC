d44b948aa3d1cb1d46ecc6fc475ce651435d49d6
Jose Dihego <josedihego@gmail.com>
2026-03-23 16:07:35 -0300

    go

--- 2026_edificacoes_1813/cod_02_imc.c
+++ 2026_edificacoes_1813/cod_02_imc.c
@@ -5,4 +5,10 @@ int main(){
     float m;
     float a;
     float imc;
+    printf("Informe sua altura: ");
+    scanf("%f",&a);
+    printf("Informe seu peso(massa) em kg: ");
+    scanf("%f",&m);
+    imc = m/(a*a);
+    printf("Seu IMC é %.1f\n",imc);
 }
