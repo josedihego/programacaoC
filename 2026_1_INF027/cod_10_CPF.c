2d61fe468cec7c20332a88347093aeeaa3d90465
josedihego@gmail.com <josedihego@gmail.com>
2026-03-31 20:19:01 -0300

    go

---
+++ 2026_1_INF027/cod_10_CPF.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int d1, d2, d3, d4, d5, d6, d7, d8, d9, v10, v11;
+    printf("Informe seu CPF para validação:");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &v10, &v11);
+    int soma1 = d1 * 10 +
+                d2 * 9 +
+                d3 * 8 +
+                d4 * 7 +
+                d5 * 6 +
+                d6 * 5 +
+                d7 * 4 +
+                d8 * 3 +
+                d9 * 2;
+    int resto1 = soma1 %11;
+    int digito_verificador_1;
+    if(resto1<2){
+        digito_verificador_1 = 0;
+    }
+    else{
+        digito_verificador_1 = 11 - resto1;
+    }
+    printf("Digito verificado 1: %d\n",digito_verificador_1);
+}
