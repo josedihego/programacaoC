7d67c86c7d0aab46b9a68f4fb58f659e96403f40
josedihego@gmail.com <josedihego@gmail.com>
2026-04-15 14:28:23 -0300

    go

--- 2026_edificacoes_1811/cod_06_bhaskara.c
+++ 2026_edificacoes_1811/cod_06_bhaskara.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(){
     //entrada
@@ -16,9 +17,9 @@ int main(){
     delta = (b*b) - (4*a*c);
     printf("Valor de delta = %.2f\n",delta);
     if(delta>=0){
-        x1 = (-b+raiz(delta))/(2*a);
-        x2 = (-b-raiz(delta))/(2*a);
-        printf("x1=%.2f e x2=%.2f\n",x1,x2);
+        x1 = (-b+sqrt(delta))/(2*a);
+        x2 = (-b-sqrt(delta))/(2*a);
+        printf("x1=%.4f e x2=%.4f\n",x1,x2);
     }
     else{
         printf("Não existem raízes reais\n");
