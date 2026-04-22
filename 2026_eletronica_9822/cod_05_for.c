126c4f1175d84cc46e9e3d3535e26a3e60d07c42
josedihego@gmail.com <josedihego@gmail.com>
2026-04-22 10:10:20 -0300

    go

--- 2026_eletronica_9822/cod_05_for.c
+++ 2026_eletronica_9822/cod_05_for.c
@@ -4,8 +4,8 @@
 int main(){
     for(int i = 0; i < 10; i= i +1){
         printf("%d\n",i);
-        sleep(1);
-        printf("\a\a");
+        //sleep(1);
+        printf("\a");
         fflush(stdout);
     }
 
