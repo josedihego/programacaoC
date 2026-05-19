63f7ee1e368fddf9704cfdfd63dce34763b7fe00
josedihego@gmail.com <josedihego@gmail.com>
2026-05-19 19:07:44 -0300

    go

--- 2026_1_INF027/cod_27_sonda_espacial.c
+++ 2026_1_INF027/cod_27_sonda_espacial.c
@@ -1,16 +1,15 @@
 /**
- * 
  * Crie um programa para monitorar os dados produzidos por uma 
- * sonda espacial. A sonda faz 30 leituras por minuto -- ou seja 1 leitura a cada 2 segundos.
- * Seu programa deve ler 30 valores -- cada valor é uma letra/caracter.
- * Essão letras poderão ser apenas:
+ * sonda espacial. A sonda faz 30 leituras por minuto -- ou seja, 1 leitura a cada 2 segundos.
+ * Seu programa deve ler 30 valores -- cada valor é uma letra/caractere.
+ * Essas letras poderão ser apenas:
  * O - oxigênio
  * C - carbono
  * N - nitrogênio
  * H - hidrogênio
  * X - não identificado
- * Ao final como saída seu programa deve responder:
+ * Ao final, como saída, seu programa deve responder:
  * - Se a amostra é válida (16 leituras ou mais diferentes de X)
- *    -> Sendo válida informar o elemento predominante
+ *    -> Sendo válida, informar o elemento predominante
  * - Se a amostra é INVÁLIDA (15 ou mais leituras com X)
  */
