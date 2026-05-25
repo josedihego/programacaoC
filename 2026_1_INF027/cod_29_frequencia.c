de76eb90a6fc8d8963301bac4aa553fdd884ff5c
josedihego@gmail.com <josedihego@gmail.com>
2026-05-25 18:33:46 -0300

    go

--- 2026_1_INF027/cod_29_frequencia.c
+++ 2026_1_INF027/cod_29_frequencia.c
@@ -18,3 +18,50 @@ o 2 aparece 3 vezes e os demais uma vez cada
 Assim a resposta deve ser 2 com frequência de 3 vezes
  
  */
+
+ #include <stdio.h>
+#include <stdlib.h>
+
+void imprimir_vetor(int valores [], int tam){
+    printf("\n<");
+    for(int p=0; p < tam-1; p = p+1){
+        printf("%d, ",valores[p]);
+    }
+    printf("%d >\n",valores[tam-1]);
+}
+#define TAM 10
+
+int main(){
+    int valores[TAM];
+    for(int p = 0; p <TAM; p = p +1){
+        printf("Informe o valor n° %d: ", p+1);
+        scanf("%d",&valores[p]);
+    }
+     for(int p=1; p < TAM;p = p+1){
+        int chave = valores[p];
+        int q = p-1;
+        while(chave < valores[q] && q>=0){
+            valores[q+1] = valores[q];
+            q = q -1;
+        }
+        valores[q+1] = chave;
+    }
+    int elemento = valores[0];
+    int frequencia = 1;
+    int vencedor = valores[0];
+    int frequencia_vencedor = 1;
+    for(int p=1; p < TAM; p = p+1){
+        if(elemento==valores[p]){
+            frequencia = frequencia +1;
+        }
+        else{
+            if(frequencia > frequencia_vencedor){
+                vencedor = elemento;
+                frequencia_vencedor = frequencia;
+            }
+            elemento = valores[p];
+            frequencia = 1;
+        }
+    }
+    printf("Elemento com maior frequência é %d que aparece %d vezes", vencedor, frequencia_vencedor);
+}
