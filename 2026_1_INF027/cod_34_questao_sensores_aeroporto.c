d2e652014bd8428723ccc2cc92cb41af1ae9404c
josedihego@gmail.com <josedihego@gmail.com>
2026-06-09 20:04:19 -0300

    go

--- 2026_1_INF027/cod_34_questao_sensores_aeroporto.c
+++ 2026_1_INF027/cod_34_questao_sensores_aeroporto.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 float media(float v1, float v2, float v3){
     float res = (v1+v2+v3)/3;
@@ -45,10 +46,46 @@ float media_descarte(float v1, float v2, float v3){
 #define NUN_SEN 2
 #define NUN_MED 3
 int main(){
-   float medidas_sensores[NUN_SEN * NUN_MED];
+   float leituras[NUN_SEN * NUN_MED];
 
-   for(int s=0; s < NUN_SEN; s= s+1){
-    printf("Informe as três leituras do sensor n° %d:",s+1);
-    scanf("%f %f %f",medidas_sensores[s], medidas_sensores[s+1], medidas_sensores[s+2]);
+   for(int s=0; s < NUN_SEN * NUN_MED; s= s+3){
+        printf("Informe as três leituras do sensor n° %d:",(s/3)+1);
+        scanf("%f %f %f",&leituras[s], &leituras[s+1], &leituras[s+2]);
    }
+
+   float soma = 0;
+
+   for(int p=0; p < NUN_SEN * NUN_MED; p = p +3){
+       soma = soma + media(leituras[p], leituras[p+1], leituras[p+2]);
+   }
+
+   printf("Média geral é %.2f\n", soma/NUN_SEN);
+
+   float soma_descartados = 0;
+   for(int p=0; p < NUN_SEN * NUN_MED; p = p +3){
+        soma_descartados = soma_descartados + media_descarte(leituras[p], leituras[p+1], leituras[p+2]);
+   }
+   printf("Média com descarte é %.2f\n", soma_descartados/NUN_SEN);
+
+   int qnt_rec ;
+   float rec;
+
+   int qnt_rec_vitoriosa = 1;
+   float rec_vitorioso = leituras[0];
+   
+   for(int p =0; p < NUN_SEN * NUN_MED; p = p +1){
+      rec = leituras[p];
+      qnt_rec = 0;
+     for(int d = 0 ; d < NUN_SEN * NUN_MED; d = d+1){
+        if(rec == leituras[d]){
+            qnt_rec = qnt_rec +1;
+        }
+     }
+     if(qnt_rec > qnt_rec_vitoriosa){
+        qnt_rec_vitoriosa = qnt_rec;
+        rec_vitorioso = rec;
+     }
+
+   }
+   printf("A temperatura mais recorrente é %.2f que aparece %d vezes.\n",rec_vitorioso, qnt_rec_vitoriosa);
 }
--- 2026_1_INF027/tempCodeRunnerFile.c
+++ 2026_1_INF027/tempCodeRunnerFile.c
@@ -1,1 +1,1 @@
-50
+nova_media
