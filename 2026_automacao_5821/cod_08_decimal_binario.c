443b8a358a99281057d1a20457418997af5e95ee
josedihego@gmail.com <josedihego@gmail.com>
2026-06-17 11:14:00 -0300

    go

--- 2026_automacao_5821/cod_08_decimal_binario.c
+++ 2026_automacao_5821/cod_08_decimal_binario.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(){
 
-    int decimal, resto;
+    int decimal, resto, copia_decimal;
     int binario[32];
     int ppl = 0;
     printf("Informe um valor natural:");
     scanf("%d",&decimal);
+    copia_decimal = decimal;
     printf("Decimal %d em binário é ", decimal);
     while(decimal !=0){
         resto = decimal % 2;
@@ -21,4 +23,22 @@ int main(){
     }
     printf("\n");
     //printf("Tamanho de um int em C é %lu bits \n", sizeof(int) * 8);
+    bool palindromo = true;
+    int meio = ppl/2;
+
+    int c = 0; // c cresce
+    int d = ppl-1;// d decresce 
+    for(c= 0; c < meio; c = c+1){
+        if(binario[c]!=binario[d]){
+            palindromo = false;
+        }
+        d = d-1;
+    }
+    if(palindromo){
+        printf("%d em binário é palindromo\n", copia_decimal);
+    }
+    else{
+        printf("%d em binário NÃO é palindromo\n", copia_decimal);
+    }
+    
 }
