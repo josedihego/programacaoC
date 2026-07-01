66d9b9342dc437d33d66c2b571d9bfe76da846a3
josedihego@gmail.com <josedihego@gmail.com>
2026-07-01 12:11:09 -0300

    go

--- 2026_automacao_5821/cod_10_kaprekar.c
+++ 2026_automacao_5821/cod_10_kaprekar.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+    int numero;                                // variavel que guarda o número
+    printf("Informe um número de 4 dígitos:"); // impressão
+    scanf("%d", &numero);                      // leitura do numero
+    int alg[4];                                // array de tamanho 4
 
-int main(){
-    int numero;//variavel que guarda o número
-    printf("Informe um número de 4 dígitos:");// impressão
-    scanf("%d",&numero);//leitura do numero
-    int alg[4];// array de tamanho 4
+    while (numero != 6174)
+    {
+        alg[0] = numero / 1000;
+        numero = numero % 1000;
+        alg[1] = numero / 100;
+        numero = numero % 100;
+        alg[2] = numero / 10;
+        alg[3] = numero % 10;
 
-    alg[0] = numero/1000;
-    numero = numero%1000;
-    alg[1] = numero/100;
-    numero = numero%100;
-    alg[2] = numero/10;
-    alg[3] = numero%10;
+        printf("pedaços %d-%d-%d-%d\n", alg[0], alg[1], alg[2], alg[3]);
 
-    printf("pedaços %d-%d-%d-%d\n",alg[0],alg[1],alg[2],alg[3]);
-
-    // agora vamos ordenar
-    for(int a=0; a < 4; a = a+1){
-        for(int b=0; b<4; b = b+1){
-            if(alg[a] < alg[b]){
-                int aux = alg[a];
-                alg[a] = alg[b];
-                alg[b] = aux;
+        // agora vamos ordenar
+        for (int a = 0; a < 4; a = a + 1)
+        {
+            for (int b = 0; b < 4; b = b + 1)
+            {
+                if (alg[a] < alg[b])
+                {
+                    int aux = alg[a];
+                    alg[a] = alg[b];
+                    alg[b] = aux;
+                }
             }
         }
+        printf("pedaços ordenados %d-%d-%d-%d\n", alg[0], alg[1], alg[2], alg[3]);
+
+        int numero_maior = alg[3] * 1000 + alg[2] * 100 + alg[1] * 10 + alg[0] * 1;
+        int numero_menor = alg[0] * 1000 + alg[1] * 100 + alg[2] * 10 + alg[3] * 1;
+        numero = numero_maior - numero_menor;
+        printf("%d - %d = %d\n", numero_maior, numero_menor, numero);
     }
-    printf("pedaços ordenados %d-%d-%d-%d\n",alg[0],alg[1],alg[2],alg[3]);
-    
 }
