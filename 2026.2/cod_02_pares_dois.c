33c28791659cd09055158753aec79b644464f91b
josedihego@gmail.com <josedihego@gmail.com>
2026-08-25 19:46:22 -0300

    go

--- 2026.2/cod_02_pares_dois.c
+++ 2026.2/cod_02_pares_dois.c
@@ -6,7 +6,7 @@ int main()
     int n1;
     int n2;
     int n3;
-    printf("Informe três valores seprados por vírgula");
+    printf("Informe três valores seprados por vírgula:");
     scanf("%d,%d,%d", &n1, &n2, &n3);
     // outra maneira, lê um por vez
     // scanf("%d",&n1);
@@ -14,32 +14,39 @@ int main()
     // scanf("%d",&n3);
     if (n1 == n2 && n3 != n2)
     { // n1==n2 && n3 != n1
-        if(n1%2==0){
+        if (n1 % 2 == 0)
+        {
             printf("SIM\n");
         }
-        else{
+        else
+        {
             printf("NÃO\n");
         }
     }
     else if (n2 == n3 && n2 != n1)
     { // n2==n3 && n3 != n1
-        if(n2%2==0){
+        if (n2 % 2 == 0)
+        {
             printf("SIM\n");
         }
-        else{
+        else
+        {
             printf("NÃO\n");
         }
     }
     else if (n1 == n3 && n3 != n2)
     {
-        if(n1%2==0){
+        if (n1 % 2 == 0)
+        {
             printf("SIM\n");
         }
-        else{
+        else
+        {
             printf("NÃO\n");
         }
     }
-    else{
+    else
+    {
         printf("NÃO\n");
     }
 }
